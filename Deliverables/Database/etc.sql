cebd78460a609f8562e39790f1d82ed9ee0837ed
Min-Chun Lo <minchun.lo@outlook.com>
2023-10-04 01:15:01 -0400

    Update time log 2023-10-03 & SQL scripts/statements & backend

--- Deliverables/Database/etc.sql
+++ Deliverables/Database/etc.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE vflashcards;
 -- \l to list all database and confirm that it has been created
 -- \c vflashcards to connect to the database
+-- \! cls to clear screen
 
 -- Create the tables:
 
@@ -27,36 +28,36 @@ DROP TABLE "USER", "VFLASHCARD", "VFLASHCARD_SET";
 -- Install uuid module so that it can be used to generate primary key
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Can verify whether DB can generate uuid by issuing either of the following statements:
+-- Verify whether DB can generate uuid by issuing either of the following statements:
 SELECT uuid_generate_v1();
 SELECT uuid_generate_v4();
 
 -- Create user table and relevant columns
-CREATE TABLE user(
-    user_id uuid DEFAULT uuid_generate_v4(),
-    user_fname character varying(127),
-    user_lname character varying(127), NOT NULL
-    user_email character varying(255) NOT NULL UNIQUE,
-    user_password character varying(255), NOT NULL
-    PRIMARY KEY(user_id)
+CREATE TABLE vfc_user (
+    vfc_user_id uuid DEFAULT uuid_generate_v4(),
+    vfc_user_fname character varying(127),
+    vfc_user_lname character varying(127) NOT NULL,
+    vfc_user_email character varying(255) NOT NULL UNIQUE,
+    vfc_user_password character varying(255) NOT NULL,
+    PRIMARY KEY(vfc_user_id)
 );
 
 -- Create vflashcard_set table with relevant columns
--- Still need to add foreign keys
 CREATE TABLE vflashcard_set(
     vfc_set_id uuid DEFAULT uuid_generate_v4(),
-    user_id uuid NOT NULL,
+    vfc_user_id uuid NOT NULL,
     vfc_set_title character varying(255) NOT NULL,
-    PRIMARY KEY (vfc_set_id, user_id)
+    PRIMARY KEY (vfc_set_id, vfc_user_id),
+    CONSTRAINT fk_vfc_user_id FOREIGN KEY(vfc_user_id) REFERENCES vfc_user(vfc_user_id)
 );
 
 
 -- Create vflashcard table
--- Still need to add foreign keys
 CREATE TABLE vflashcard (
-    vfc_id uuid NOT NULL,
+    vfc_id uuid DEFAULT uuid_generate_v4(),
     vfc_set_id uuid NOT NULL,
     vfc_question character varying(255) NOT NULL,
     vfc_answer character varying(255) NOT NULL,
-    PRIMARY KEY (vfc_id, vfc_set_id)
+    PRIMARY KEY (vfc_id, vfc_set_id),
+    CONSTRAINT fk_vfc_set_id FOREIGN KEY(vfc_set_id) REFERENCES vflashcard_set(vfc_set_id)
 );
