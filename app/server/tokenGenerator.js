6c35c7a883a3ddc04076691a8d24f874a735fc42
Min-Chun Lo <minchun.lo@outlook.com>
2023-11-04 12:53:27 -0400

    Added notes/draft/screenshots for the Presentation Draft. Updated time log.

--- app/server/tokenGenerator.js
+++ app/server/tokenGenerator.js
@@ -8,11 +8,9 @@ function tokenGenerator(vfc_user_id) {
     // return token.sign({login_id: vfc_user_id}, secret, { expiresIn: "1h" });
     return token.sign({ vfc_user_id }, secret, { expiresIn: "1h" });
 
-
     // Following lines do not work 
     // return token.sign(JSON.stringify(vfc_user_id), secret, { expiresIn: "1h"});
     // return token.sign(vfc_user_id, secret, { expiresIn: "1h" });
-
 }
 
 module.exports = tokenGenerator;
