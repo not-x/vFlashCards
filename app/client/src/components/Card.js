48fbba897d2820d5f231e8a8878b9ecdbe7ed8cf
Min-Chun Lo <minchun.lo@outlook.com>
2023-12-08 23:33:27 -0500

    Added toggle function to Card

--- app/client/src/components/Card.js
+++ app/client/src/components/Card.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-// Question/front of the card should display the same card but with the response
-// Navbar needs to be added to the bottom of the card
+
 function Card({ vfc_set_title, vfc_set_id, vfc_id, vfc_question, vfc_answer }) {
+
+  const [side, setSide] = useState(vfc_question);
+  // console.log("Current side: " + side + " Type: " + typeof(side))
+  // console.log("typeof(vfc_answer): " + typeof(vfc_answer));
+  const handleClick = (e) => {
+    if (side === vfc_question) {
+      console.log("set to answer")
+      setSide( vfc_answer )
+    } else {
+      console.log("set to question")
+      setSide( vfc_question )
+    }
+    console.log("Card has been set to: " + side)
+
+  }
+
+
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text">
           {/* <Link to={"/profile/lib/" + vfc_set_id}>{vfc_question}</Link> */}
+          {side}
         </div>
+        <button className="btn btn-outline-secondary btn-sm" onClick={handleClick}>Flip Card</button>
+
         <div className="card-footer small text-muted text-end">Card#: {vfc_id}</div>
       </div>
     </div>
