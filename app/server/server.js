28bd7b8aab96425a6f681ccfed785d92b70472d2
Min-Chun Lo <minchun.lo@outlook.com>
2023-12-12 16:10:19 -0500

    Updated server.js to unknown routes to the react app.

--- app/server/server.js
+++ app/server/server.js
@@ -10,6 +10,11 @@ app.use(cors());
 app.use("/auth", require("./routes/login"));
 app.use("/profile", require("./routes/userProfile"));
 
+// redirect all unknown routes to react app
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
